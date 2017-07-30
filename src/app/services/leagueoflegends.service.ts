afd254b225f8afdb1d2f106df58550597febde0c
Connor Lee <connorlee@u.boisestate.edu>
2017-07-30 17:09:46 -0600

    Fix missing injectable attribute on LeagueOfLegendsService

--- src/app/services/leagueoflegends.service.ts
+++ src/app/services/leagueoflegends.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpModule, RequestOptions, Request } from '@angular/http'
 import { Champion } from '../components/champion/champion-detail.component';
 
+@Injectable()
 export class LeagueOfLegendsService {
     getChampions(): Champion[] {
         return; 
